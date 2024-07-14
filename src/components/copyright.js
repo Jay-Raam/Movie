1faa0cd50917cd9a8c2be222c1890969d4d77b9b
Jayasriraam-s <jayasriraam27@gmail.com>
2024-07-14 22:18:22 +0530

    change css footer

--- src/components/copyright.js
+++ src/components/copyright.js
@@ -4,7 +4,7 @@ const Copyright = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="mt-10 lg:mt-20 border-t-2 border-gray-300">
+    <footer className="mt-10 lg:mt-[25rem] border-t-2 border-gray-300">
       <p className="text-center text-gray-700 py-2">
         &copy; {currentYear} All rights reserved by Jayasriraam
       </p>
